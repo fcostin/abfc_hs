42fd3412a9b626a1686bf58b0bf49c298bcb0ae9
Reuben Fletcher-Costin <reuben.fletchercostin@gmail.com>
2012-04-17 18:44:46 +1000

    only print compiled opcodes when run

--- main.hs
+++ main.hs
@@ -8,25 +8,12 @@ import Abfc.Macros (LMacro, LStatement)
 import Abfc.Eval (evaluate)
 
 
-
-print_list body = do
-    mapM_ putStrLn (map show body)
-
 do_compile parsed_program = do
     let macros = convert_program parsed_program
     let macros' = compile_macro_program_alpha macros
     let main_body = compile_macro_program_beta macros'
-    let code = evaluate main_body
-    putStrLn ">>> parsed macros:"
-    putStrLn (show parsed_program)
-    putStrLn ">>> converted macros:"
-    putStrLn (show macros)
-    putStrLn ">>> phase alpha output:"
-    putStrLn (show macros')
-    putStrLn ">>> phase beta output:"
-    print_list main_body
-    putStrLn ">>> evaluation output:"
-    print_list code 
+    let code = concat $ evaluate main_body
+    putStr code
 
 -- [main entry point]
 main = do
@@ -34,4 +21,4 @@ main = do
     case parse_macro_program macro_source of
         Left e -> putStrLn ("parse error: " ++ (show e))
         Right x -> do_compile x
-    putStrLn "done."
+    putStrLn ""
